51fff6079af07652126dc627c2e407b994103752
acrylic-style <me@acrylicstyle.xyz>
2020-10-29 13:51:12 +0900

    1.16.4-rc1

--- src/main/java/xyz/acrylicstyle/mcutil/lang/MCVersion.java
+++ src/main/java/xyz/acrylicstyle/mcutil/lang/MCVersion.java
@@ -11,7 +11,8 @@ import java.util.Map;
 
 @SuppressWarnings({ "unused", "RedundantSuppression" })
 public enum MCVersion {
-    v1_16_4(0x40000003, 2583, true, "1.16.4"), // unreleased version
+    v1_16_4(0x40000004, 2584, true, "1.16.4"), // unreleased version
+    v1_16_4_RC1(0x40000003, 2583, true, "1.16.4-rc1"),
     v1_16_4_PRE2(0x40000002, 2582, true, "1.16.4-pre2"),
     v1_16_4_PRE1(0x40000001, 2581, true, "1.16.4-pre1"), // New network protocol scheme, with a high bit (bit 30) set for snapshots.
     v1_16_3(753, 2580, true, "1.16.3"),
